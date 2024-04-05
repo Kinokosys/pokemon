ce769de896c7a9620233122bd6205a56ac435b50
Ines Mzyene <emmawasnotthere@gmail.com>
2024-04-05 11:03:49 +0200

    second

--- src/PokemonFeu.kt
+++ src/PokemonFeu.kt
@@ -1,2 +1,27 @@
 class PokemonFeu {
+    var nom = ""
+    var hp = 0
+    var atk = 0
+
+    constructor(nom: String, hp: Int, atk: Int) {
+        this.nom = nom
+        this.hp = hp
+        this.atk = atk
+    }
+
+    fun isDead(): Boolean {
+        if (this.hp == 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    fun attaquer(p) {
+        if (p == PokemonPlante(p)) {
+            var atk *= 2
+        } else if (p == PokemonEau(p) || PokemonFeu(p)) {
+            var atk *= 0.5
+        }
+    }
 }
--- src/PokemonPlante.kt
+++ src/PokemonPlante.kt
@@ -1,2 +1,19 @@
 class PokemonPlante {
+    var nom = ""
+    var hp = 0
+    var atk = 0
+
+    constructor(nom: String, hp: Int, atk: Int) {
+        this.nom = nom
+        this.hp = hp
+        this.atk = atk
+    }
+
+    fun isDead(): Boolean {
+        if (this.hp == 0) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
